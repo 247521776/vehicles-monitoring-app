ed5b841d01439d8940a7b4fc7f309cd9dec08441
abdotalaat <abdotalaatfcih@gmail.com>
2019-09-03 16:31:51 +0400

    CI/CD

---
+++ demo/src/main/java/com/example/demo/TestClass.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class TestClass {
+
+
+
+
+
+    @GetMapping
+    public ResponseEntity<String> findAll() {
+
+        return ResponseEntity.ok("ABdo");
+    }
+
+
+
+
+}
